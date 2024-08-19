7fc380b01c4e2ad2f181f0f09567f572e55c9a34
Ulises Ochoa <ulisesochoap@gmail.com>
2024-08-19 00:38:21 -0500

    Correcciones

--- src/views/Assignment/Assignment.vue
+++ src/views/Assignment/Assignment.vue
@@ -4,45 +4,21 @@ import { Plus } from '@element-plus/icons-vue'
 import apiClient from '@/plugins/axios/axios'
 
 
-const customer = ref([]);
+const assignment = ref([])
 
 const loadData = async () => {
   const response = await apiClient.get('/customers')
-  console.log(response);
-
-
-};
-
-const tableData = [
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles'
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles'
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles'
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles'
-  }
-]
+  const { data } = response.data
+  assignment.value = data
+}
 
 onMounted(() => {
   loadData()
-});
+})
 </script>
 
 <template>
-  <el-table :data="tableData" style="width: 100%">
+  <el-table :data="assignment" style="width: 100%">
     <el-table-column fixed="right" align="center">
       <template #header>
         <el-row justify="space-evenly" align="middle">
@@ -71,8 +47,8 @@ onMounted(() => {
           </el-col>
         </el-row>
       </template>
-      <el-table-column prop="client_name" label="Nombre del cliente" />
-      <el-table-column prop="gas_quality_anme" label="Calidad" />
+      <el-table-column prop="first_name" label="Nombre del cliente" />
+      <el-table-column prop="quality" label="Calidad" />
       <el-table-column prop="purchase_price" label="Precio de compra" />
       <el-table-column prop="sale_price" label="Precio de venta" />
       <el-table-column prop="profit" label="Beneficio" />
